0b69cef24e01b815ab5d05fad7424b9acc9839f0
yahya.bahhouss <yahiabahhous@gmail.com>
2023-09-27 15:38:49 +0300

    Update - 1

--- src/main/java/com/example/quizsurveyapp/controllers/portfolio/MessageController.java
+++ src/main/java/com/example/quizsurveyapp/controllers/portfolio/MessageController.java
@@ -5,6 +5,8 @@ import com.example.quizsurveyapp.mapper.portfolio.MessageMapper;
 import com.example.quizsurveyapp.models.portfolio.Message;
 import com.example.quizsurveyapp.services.portfolio.MessagesService;
 import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/msg")
 @CrossOrigin(origins = "*" )
+@Slf4j
 public class MessageController {
     @Autowired
     private MessagesService messagesService;
@@ -24,17 +27,20 @@ public class MessageController {
 
     @PostMapping
     public ResponseEntity<MessageDTO> createMessage (@Valid @RequestBody MessageDTO messageDTO){
+        log.debug("Save msg in DB");
         var message = messagesService.saveMessage(messageMapper.toEntity(messageDTO));
         return new ResponseEntity<>(messageMapper.toDTO(message), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<MessageDTO> getMessageByID (@PathVariable long id){
+        log.debug("Get msg by id from DB");
         return new ResponseEntity<>(messageMapper.toDTO(messagesService.getMessageById(id)),HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<List<MessageDTO>> getAllMessages (){
+        log.debug("Get all msgs");
         List<Message> messages = messagesService.getAllMessages();
         return new ResponseEntity<>(messages.stream().map(messageMapper::toDTO).collect(Collectors.toList()), HttpStatus.OK);
     }
--- src/main/java/com/example/quizsurveyapp/services/portfolio/MessagesService.java
+++ src/main/java/com/example/quizsurveyapp/services/portfolio/MessagesService.java
@@ -4,6 +4,7 @@ package com.example.quizsurveyapp.services.portfolio;
 import com.example.quizsurveyapp.exception.ResourceNotFoundException;
 import com.example.quizsurveyapp.models.portfolio.Message;
 import com.example.quizsurveyapp.repositories.portfolio.MessagesRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +12,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class MessagesService {
     @Autowired
     private MessagesRepository messagesRepository;
 
     public Message saveMessage (Message message){
+        log.debug("Save msg in DB - Service class");
         return messagesRepository.save(message);
     }
 
     public Message getMessageById (long id ){
+        log.debug("Get msg by id from DB - Service class");
         return messagesRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Message not found"));
     }
 
@@ -32,6 +36,7 @@ public class MessagesService {
     }
 
     public List<Message> getAllMessages (){
+        log.debug("Get all msgs - Service class");
         return messagesRepository.findAll();
     }
 }
