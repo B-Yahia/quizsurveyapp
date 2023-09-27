fe5d68ad1bb9eb99ed884bdf9b2cbf6971dcdc12
yahya.bahhouss <yahiabahhous@gmail.com>
2023-09-27 18:17:58 +0300

    Update - 5

--- src/main/java/com/example/quizsurveyapp/security/CorsConfig.java
+++ src/main/java/com/example/quizsurveyapp/security/CorsConfig.java
@@ -9,17 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-//                .allowedHeaders("*");
-//    }
-
     @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
+
+
 }
